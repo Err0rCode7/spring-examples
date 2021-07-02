6f0b835ca172db4ec96e599c9869b61b5a9cfd8a
Err0rCode7 <aa6184708@naver.com>
2021-07-02 20:51:41 +0900

    # Add Json ObjectMapper bean

---
+++ spring-redis/src/main/java/com/redis/spring/config/RedisConfig.java
@@ -0,0 +1,49 @@
+package com.redis.spring.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.redis.spring.domain.MyData;
+import org.msgpack.jackson.dataformat.MessagePackFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        return lettuceConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, byte[]> redisTemplate() {
+        RedisTemplate<String, byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory((redisConnectionFactory()));
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setEnableDefaultSerializer(false);
+        return redisTemplate;
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        return template;
+    }
+
+    @Bean
+    public ObjectMapper messagePackObjectMapper() {
+        return new ObjectMapper(new MessagePackFactory())
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
+    }
+}
